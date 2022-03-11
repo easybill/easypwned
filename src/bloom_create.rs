28b24de0f4aa7072502863b43a0d7a9bf243e67b
timglabisch <glabisch@easybill.de>
2022-03-11 08:58:14 +0100

    stash changes

--- src/bloom_create.rs
+++ src/bloom_create.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader, Read, Write};
 use anyhow::{anyhow, Error};
 use bloomfilter::Bloom;
 
@@ -13,6 +13,29 @@ fn count_lines(path : &str) -> Result<usize, Error> {
     }
 
     Ok(i)
+}
+
+pub fn bloom_get() -> Result<Bloom<Vec<u8>>, Error> {
+
+    let mut file = File::open("easypwned.bloom")?;
+    let mut buf = vec![];
+    file.read_to_end(&mut buf)?;
+
+
+    Ok(Bloom::from_existing(
+        &buf,
+        8120685768,
+        7, [(16911278473676693785, 7977664235212237539), (6990288496210676087, 3334793689188451176)]
+    ))
+
+    /*
+    Bloom::from_existing(
+        &original.bitmap(),
+        original.number_of_bits(),
+        original.number_of_hash_functions(),
+        original.sip_keys(),
+    );
+     */
 }
 
 pub fn bloom_create() -> Result<(), Error> {
@@ -41,8 +64,16 @@ pub fn bloom_create() -> Result<(), Error> {
             _ => return Err(anyhow!("invalid hash pattern"))
         };
 
-        bloom.set(hash);
+        bloom.set(hash.as_bytes());
     }
+
+    // number_of_bits: 8120685768, number_of_hash_functions: 7, sip_keys: [(16911278473676693785, 7977664235212237539), (6990288496210676087, 3334793689188451176)]
+    println!("number_of_bits: {:?}, number_of_hash_functions: {:?}, sip_keys: {:?}",
+        bloom.number_of_bits(),
+        bloom.number_of_hash_functions(),
+        bloom.sip_keys(),
+    );
+    // panic!();
 
     let mut bloomfile = File::create("easypwned.bloom")?;
     bloomfile.write_all(bloom.bitmap().as_ref())?;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,64 @@
-use crate::bloom_create::bloom_create;
+use crate::bloom_create::{bloom_create, bloom_get};
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    response::IntoResponse,
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+use axum::extract::Extension;
 
 pub mod bloom_create;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+
+
     match bloom_create() {
         Ok(()) => {},
         Err(e) => println!("could not create bloom: {}", e),
     };
 
-    println!("Hello, world!");
+    let bloom = match bloom_get() {
+        Ok(b) => b,
+        Err(e) => {
+            println!("could not get bloom {}", e);
+            panic!();
+        }
+    };
+
+    let checks = vec![
+        "0000000CAEF405439D57847A8657218C618160B2"
+    ];
+
+    for check in checks {
+        println!("check: {} -> {:?}", check, bloom.check(&check.as_bytes().to_vec()));
+    }
+
+    panic!("ok!");
+
+
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        // .layer(Extension())
+        .route("/", get(root));
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
 }
